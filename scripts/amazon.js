16c57a2c825faedf6b732420a19784233857ca7f
mazenDev <mazenfathy841@gmail.com>
2025-02-08 21:09:26 +0200

    Useing products.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,36 +1,4 @@
-const productGrid = document.querySelector('.js-prodcts-grid')
-
-const products = [
-  {
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      stars: 4.5,
-      count: 87,
-    },
-    priceCentes: 1090,
-  },
-
-  {
-    image: "images/products/intermediate-composite-basketball.jpg",
-    name: "Intermediate Size Basketball",
-    rating: {
-      stars: 4,
-      count: 127,
-    },
-    priceCentes: 2095,
-  },
-
-  {
-    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
-    rating: {
-      stars: 4.5,
-      count: 56,
-    },
-    priceCentes: 799,
-  },
-];
+const productGrid = document.querySelector(".js-prodcts-grid");
 
 function generateHtml(list = []) {
   let html = ` `;
@@ -49,14 +17,14 @@ function generateHtml(list = []) {
 
           <div class="product-rating-container">
             <img class="product-rating-stars"
-              src="images/ratings/rating-${product.rating.stars*10}.png">
+              src="images/ratings/rating-${product.rating.stars * 10}.png">
             <div class="product-rating-count link-primary">
               ${product.rating.count}
             </div>
           </div>
 
           <div class="product-price">
-            $${(product.priceCentes / 100).toFixed(2)}
+            $${(product.priceCents / 100).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
