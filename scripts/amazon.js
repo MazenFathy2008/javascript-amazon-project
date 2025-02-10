4ad2fe05ec6a1da550a61a23806ad43697ba04c3
mazenDev <mazenfathy841@gmail.com>
2025-02-10 21:29:16 +0200

    organize the code by modules

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,76 +1,17 @@
+// import needed files
+import { products } from "../data/products.js"; // --> data
+import { cart } from "../data/cart.js"; // --> data
+import { generateHtml } from "./functions/generating-html.js"; // --> function
+import { addButtonFunc } from "./functions/add-button-func.js"; // --> function
+///////////////////////////////////////////////
+/* code for generating page */
 // put the grid element in the js
 const productGridElement = document.querySelector(".js-prodcts-grid");
-
-// this used to generate html for page
-function generateHtml(list = []) {
-  let html = ` `;
-  list.forEach((product) => {
-    html += `
-    <div class="product-container">
-          <div class="product-image-container">
-            <img class="product-image" src="${product.image}" />
-          </div>
-
-          <div class="product-name limit-text-to-2-lines">${product.name}</div>
-
-          <div class="product-rating-container">
-            <img
-              class="product-rating-stars"
-              src="images/ratings/rating-${product.rating.stars * 10}.png"
-            />
-            <div class="product-rating-count link-primary">
-              ${product.rating.count}
-            </div>
-          </div>
-
-          <div class="product-price">
-            $${(product.priceCents / 100).toFixed(2)}
-          </div>
-
-          <div class="product-quantity-container">
-            <select class="js-select-quantatiy">
-              <option selected value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-              <option value="10">10</option>
-            </select>
-          </div>
-
-          <div class="product-spacer"></div>
-          <div class="add-button-flexbox">
-            <div class="added-to-cart">
-              <img src="images/icons/checkmark.png" />
-              Added
-            </div>
-            <div class="Added-animtion-flexbox">
-              <div class="Added-animtion js-Added-animtion">
-                <img
-                  src="images/icons/checkmark.png"
-                  alt="checkmark"
-                  class="checkmark-img"
-                />
-                <span class="Added">Added</span>
-              </div>
-            </div>
-            <button
-              class="add-to-cart-button button-primary js-add-button"
-              data-product-id="${product.id}"
-            >
-              <!-- this add data to this button to be able to use it later -->
-              Add to Cart
-            </button>
-          </div>
-        </div>
-    `;
-  });
-  return html;
-}
 // render html
 let Html = generateHtml(products);
 productGridElement.innerHTML = Html;
+
+///////////////////////////////////////////////
+/* cart code */
+addButtonFunc(cart)
+console.log(cart)
